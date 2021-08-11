ebb422ee9130bb87d14d25ec695bde99fa98c3a5
Dealmi <56388298+Dealmi@users.noreply.github.com>
2021-08-12 00:03:03 +0300

    Example of TF service file

--- service_example.tf
+++ service_example.tf
@@ -1,1 +1,46 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {} # Create a Docker Provider
+
+
+# resource "docker_image" "nginx" { # Create a Docker Image
+#   name = "nginx:alpine"
+# }
+
+#resource "docker_container" "my_container" { # Creates a Docker Container
+#  image = docker_image.alpine.latest         # Using same image which we created earlier
+#  name  = "alp_container"
+#}
+
+resource "docker_service" "web" { # Create a Docker Service
+  name = "nginx_alpine"
+  task_spec {
+    container_spec {
+      image = "nginx:alpine"  # Using same image which we created eearlier
+
+
+    }
+
+
+  }
+  mode {
+    replicated {
+      replicas = 2
+    }
+  }
+
+  endpoint_spec {
+    ports {
+      target_port    = "80"
+      published_port = "8080"
+    }
+  }
+}
 
